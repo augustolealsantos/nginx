c7fbc6146cfbfe03bd810513fa610abb89f803ed
Augusto Leal Santos <augusto_leal@outlook.com>
2021-11-30 17:29:20 -0300

    change in the log message format

--- src/http/modules/ngx_http_limit_req_module.c
+++ src/http/modules/ngx_http_limit_req_module.c
@@ -267,7 +267,7 @@ ngx_http_limit_req_handler(ngx_http_request_t *r)
 
         if (rc == NGX_BUSY) {
             ngx_log_error(lrcf->limit_log_level, r->connection->log, 0,
-                        "limiting requests%s, excess: %ui.%03ui by zone \"%V\" key[\"%V\"]",
+                        "limiting requests%s, excess: %ui.%03ui by zone \"%V\" key \"%V\"",
                         lrcf->dry_run ? ", dry run" : "",
                         excess / 1000, excess % 1000,
                         &limit->shm_zone->shm.name,
@@ -300,7 +300,7 @@ ngx_http_limit_req_handler(ngx_http_request_t *r)
     }
 
     ngx_log_error(lrcf->delay_log_level, r->connection->log, 0,
-                  "delaying request%s, excess: %ui.%03ui, by zone \"%V\" key[\"%V\"]",
+                  "delaying request%s, excess: %ui.%03ui, by zone \"%V\" key \"%V\"",
                   lrcf->dry_run ? ", dry run" : "",
                   excess / 1000, excess % 1000, &limit->shm_zone->shm.name, &ctx->key.value);
 
